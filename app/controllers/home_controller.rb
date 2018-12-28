aeff01f2c3f42cbb14506f3218f719a1bb6eadbd
Taishi Eguchi <taishi2060@gmail.com>
2018-12-28 15:29:14 +0900

    ダミーデータを生成した

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @users = User.all.includes(:fee)
   end
 end
--- app/models/fee.rb
+++ app/models/fee.rb
@@ -1,3 +1,8 @@
 class Fee < ApplicationRecord
   belongs_to :user
+  validates :month, presence: true
+  validates :people_number, presence: true
+  validates :electricity, presence: true
+  validates :gas, presence: true
+  validates :water, presence: true
 end
