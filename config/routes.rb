9a4e6115de1cdadbaea5ff9748361380f4ef45bb
Taishi Eguchi <taishi2060@gmail.com>
2018-12-28 11:34:41 +0900

    バグの解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
-  root to: "home#index"
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'
